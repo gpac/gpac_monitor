707bd2d0cf34cce70147ee0648c55e479cba2efd
PierreDillard <7gaspard77@gmail.com>
2025-07-17 16:21:21 +0200

    feat: add initial HTML template for GPAC Graph Monitor && update vite.config to display correctly html static file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Relative paths for double-click
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'), 
+    },
+  },
+  build: {
+    assetsInlineLimit: 100000000, // Inline all assets
+    cssCodeSplit: false, // Single CSS file
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Single JS file
+      }
     }
   }
-});
+})
