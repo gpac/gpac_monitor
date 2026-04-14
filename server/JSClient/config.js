99d3431d6fc1acdfebb970136450a07e31304fe8
PierreDillard <7gaspard77@gmail.com>
2026-04-14 08:58:45 +0200

    refator: remove unused variable

--- server/JSClient/config.js
+++ server/JSClient/config.js
@@ -2,12 +2,6 @@ const DEFAULT_FILTER_FIELDS = [
     "idx", "status", "bytes_done", "bytes_sent", "pck_sent", "pck_done", "time", "nb_ipid", "nb_opid", "errors", "current_errors"
 ];
 
-const CPU_STATS_FIELDS = [
-    "total_cpu_usage", "process_cpu_usage",
-    "process_memory", "physical_memory", "physical_memory_avail",
-    "gpac_memory", "thread_count",
-];
-
 const FILTER_PROPS_LITE = [
     'name', 'status', 'bytes_done', 'type', 'ID', 'nb_ipid', 'nb_opid', 'idx', 'itag', 'pck_sent', 'pck_done', 'time',"current_errors"
 ];
@@ -40,7 +34,6 @@ const LOG_RETENTION = {
 
 export {
     DEFAULT_FILTER_FIELDS,
-    CPU_STATS_FIELDS,
     FILTER_PROPS_LITE,
     FILTER_ARGS_LITE,
     PID_PROPS_LITE,
