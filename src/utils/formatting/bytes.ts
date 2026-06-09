7fba96c2daf4673f3dc6fa4ab721ba191c783354
PierreDillard <7gaspard77@gmail.com>
2026-06-09 12:57:34 +0200

    fix: update byte formatting precision to two decimal places

--- src/utils/formatting/bytes.ts
+++ src/utils/formatting/bytes.ts
@@ -7,5 +7,5 @@ export const formatBytes = (bytes: number): string => {
   const k = 1024;
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(1)) + ' ' + sizes[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 };
