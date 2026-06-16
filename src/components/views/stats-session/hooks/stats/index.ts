38311b65458769e61cecf08560844a1ce87648fa
PierreDillard <7gaspard77@gmail.com>
2026-06-16 09:57:44 +0200

    chore(worker): remove dead status enrichment worker

--- src/components/views/stats-session/hooks/stats/index.ts
+++ src/components/views/stats-session/hooks/stats/index.ts
@@ -1,4 +1,3 @@
-export { useEnrichedStats } from './useEnrichedStats';
 export { useFilterStats } from './useFilterStats';
 export { useSessionStats } from './useSessionStats';
 export { useStatsCalculations } from './useStatsCalculations';
