b18aba912377d5a2ace8768044e13449c6f2958d
PierreDillard <7gaspard77@gmail.com>
2026-06-30 09:37:32 +0200

    feat(RuntimeDetailsSection): enhance layout responsiveness based on detachment status

--- src/components/views/stats-session/monitored_filters/tabs/RuntimeDetailsSection.tsx
+++ src/components/views/stats-session/monitored_filters/tabs/RuntimeDetailsSection.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import { formatBytes, formatNumber } from '@/utils/formatting';
+import { useIsDetached } from '../FilterViewContext';
 import { MetricRow, TableSection } from './shared/tableLayout';
 
 export interface ProcessingValues {
@@ -17,31 +18,32 @@ interface RuntimeDetailsSectionProps {
 }
 
 const RuntimeDetailsSection = memo(
-  ({ processing }: RuntimeDetailsSectionProps) => (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-2 items-start">
-      <TableSection title="Processing">
-        <MetricRow
-          label="Filter Process speed"
-          value={processing.processSpeed}
-        />
-        <MetricRow label="Packets/s" value={processing.processPacketRate} />
-      </TableSection>
-      <TableSection title="Packets">
-        <MetricRow label="Done" value={formatNumber(processing.pckDone)} />
-        <MetricRow label="Sent" value={formatNumber(processing.pckSent)} />
-        {processing.pckIfceSent !== undefined && (
-          <MetricRow
-            label="Interface"
-            value={formatNumber(processing.pckIfceSent)}
-          />
-        )}
-      </TableSection>
-      <TableSection title="Data">
-        <MetricRow label="Done" value={formatBytes(processing.bytesDone)} />
-        <MetricRow label="Sent" value={formatBytes(processing.bytesSent)} />
-      </TableSection>
-    </div>
-  ),
+  ({ processing }: RuntimeDetailsSectionProps) => {
+    const isDetached = useIsDetached();
+    const gridClass = isDetached ? 'grid-cols-2' : 'grid-cols-3';
+    return (
+      <div className={`grid ${gridClass} gap-2 items-start`}>
+        <TableSection title="Processing">
+          <MetricRow label="Process speed" value={processing.processSpeed} />
+          <MetricRow label="Packets/s" value={processing.processPacketRate} />
+        </TableSection>
+        <TableSection title="Packets">
+          <MetricRow label="Done" value={formatNumber(processing.pckDone)} />
+          <MetricRow label="Sent" value={formatNumber(processing.pckSent)} />
+          {processing.pckIfceSent !== undefined && (
+            <MetricRow
+              label="Interface"
+              value={formatNumber(processing.pckIfceSent)}
+            />
+          )}
+        </TableSection>
+        <TableSection title="Data">
+          <MetricRow label="Done" value={formatBytes(processing.bytesDone)} />
+          <MetricRow label="Sent" value={formatBytes(processing.bytesSent)} />
+        </TableSection>
+      </div>
+    );
+  },
 );
 
 RuntimeDetailsSection.displayName = 'RuntimeDetailsSection';
